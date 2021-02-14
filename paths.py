eeae2d03a725a692384a5538e75d2a104edec108
Smitten <77771103+C84186@users.noreply.github.com>
2021-02-14 21:48:41 +1100

    use logging in paths

--- paths.py
+++ paths.py
@@ -2,6 +2,8 @@ import yaml, re
 from pathlib import Path, PureWindowsPath, PurePosixPath
 import defs
 
+L = logging.getLogger(__name__)
+
 def read_path_spec(spec_file_path = defs.credentials_path):
     
     with open(spec_file_path, "r") as f:
@@ -10,29 +12,29 @@ def read_path_spec(spec_file_path = defs.credentials_path):
     specs = cfg['path_bindings']
 
     if not isinstance(specs, list): 
-        print(f"Must supply a list for 'path_bindings: in {spec_file_path}'")
+        L.error(f"Must supply a list for 'path_bindings: in {spec_file_path}'")
         exit(1)
 
     if len(specs) > 1:
-        print(f"path_bindings lists can only have one set of remote / local bindings for now")
+        L.error(f"path_bindings lists can only have one set of remote / local bindings for now")
         exit(1)
 
     specs = specs[0]
 
     allowed_keys = {'local', 'remote'}
     if set(specs.keys()) != allowed_keys:
-        print("invalid top level keys for path_bindings, {specs.keys}, expecting {allowed_keys}")
+        L.error("invalid top level keys for path_bindings, {specs.keys}, expecting {allowed_keys}")
         exit(1)
 
     allowed_contexts = {'transfer', 'qbittorrent'}
     for k in specs:
         # for now, demand a dictionary
         if not isinstance(specs[k], dict):
-            print(f"expecting a dict for {k}")
+            L.error(f"expecting a dict for {k}")
             exit(1)
 
         if set(specs[k].keys()) != allowed_contexts:
-            print(f"expecting {allowed_contexts} for {k}")
+            L.error(f"expecting {allowed_contexts} for {k}")
             exit(1)
 
         for context in specs[k]:
@@ -40,12 +42,12 @@ def read_path_spec(spec_file_path = defs.credentials_path):
             kind = type(specs[k][context]) 
             if not kind in [str, dict]:
                 specs[k][context] = PurePosixPath(specs[k][context])
-                print(f"expecting a dict or str for {k}.{context}, got {kind}")
+                L.error(f"expecting a dict or str for {k}.{context}, got {kind}")
             
             if kind == dict:
                 allowed_context_keys = {'path', 'os'}
                 if set(specs[k][context].keys()) != allowed_context_keys:
-                    print(f"path_bindings[{k}][{context}] {specs[k][context].keys()}, expected {allowed_context_keys}")
+                    L.error(f"path_bindings[{k}][{context}] {specs[k][context].keys()}, expected {allowed_context_keys}")
                     exit(1)
 
                 if specs[k][context]['os'].lower().startswith("w"):
@@ -72,7 +74,7 @@ def translate_paths(fastresume_local_path, path_spec):
 
 
     if not fastresume_local_path.is_relative_to(entry['local']['qbittorrent']):
-        print(f"Unable to get {fast_resume_local_path} as relative to {path_spec_entry['local']['qbittorrent']}")
+        L.error(f"Unable to get {fast_resume_local_path} as relative to {path_spec_entry['local']['qbittorrent']}")
         return None
 
     relative_path = fastresume_local_path.relative_to(entry['local']['qbittorrent'])
