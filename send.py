c716c4759f1fdacd335d969853b1e529c0080e0c
Smitten <77771103+C84186@users.noreply.github.com>
2021-02-15 16:12:33 +1100

    initialize sftp connection, and (almost) send actual files

--- send.py
+++ send.py
@@ -8,6 +8,14 @@ L = logging.getLogger(__name__)
 path_specs = paths.read_path_spec()
 bt_backup_p = paths.get_bt_backup_path()
 
+
+progress_widgets = [progressbar.DataSize, progressbar.FileTransferSpeed, progressbar.Bar]
+
+
+def update_progress(total_transferred, total_size, progress = None):
+    if progress: progress.update(total_transferred)
+
+
 fastresumes = fastresume.parse_all_fastresumes(bt_backup_p)
 
 qbt_client = helpers.connect_client()
@@ -18,7 +26,13 @@ ssh_details = helpers.get_ssh_creds()
 ssh_client = paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
+ssh_client.connect(
+    hostname = ssh_details['hostname'],
+    port = ssh_details['port'],
+    username = ssh_details['username'],
+    key_filename = map(str, ssh_details['key_file_path']))
 
+sftp = ssh_client.open_sftp()
 
 counts = {
     'finished': 0,
@@ -63,8 +77,22 @@ for fr in fastresumes:
         file_path_local = transfer_paths['local'] / rename_path_rel
         file_path_remote = transfer_paths['remote'] / file_path_rel
 
+        L.info(f"Sending: {file_path_local} => {file_path_remote}")
 
-        if rename: L.debug(f"Sending: {file_path_local} => {file_path_remote}")
+        if not file_path_local.exists():
+            L.warn(f"File: {file_path_local} not found!")
+            continue
+
+        actual_path_size = file_path_local.stat().st_size
+        expected_path_size = torrent_file.length
+
+        if actual_path_size != expected_path_size:
+            L.warn(f"File Size Mismatch: {file_path_local} actual: {actual_path_size} != expected: {expected_path_size}")
+            continue
+        L.info(f"for {file_path_local} sizes seem to match")
+
+        #with progressbar.ProgressBar(widgets = progress_widgets , max_value = torrent_file.length) as progress:
+            # sftp.put(file_path_local, file_path_remote, callback = update_progress, confirm = True)
 
 
 print(f"{counts}")
