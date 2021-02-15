b0853c9a9ac55311927a63992d787e4c26bf9db0
Smitten <77771103+C84186@users.noreply.github.com>
2021-02-15 21:00:31 +1100

    move most of sending work to own function

--- send.py
+++ send.py
@@ -15,6 +15,67 @@ progress_widgets = [progressbar.DataSize, progressbar.FileTransferSpeed, progres
 def update_progress(total_transferred, total_size, progress = None):
     if progress: progress.update(total_transferred)
 
+def transfer_fastresumes(fastresume_list, remote_hashes, sftp_client, path_specs = paths.read_path_spec()):
+    counts = {
+        'finished': 0,
+        'progressing': 0,
+        'fastresume_problem': 0,
+        'total_tried': 0,
+        'missing_local_torrent': 0
+        }
+
+    for fr in fastresume_list:
+        counts['total_tried'] += 1
+        if not fr:
+            counts['fastresume_problem'] += 1
+            continue
+        if not 'torrent' in fr or not fr['torrent']:
+            counts['missing_local_torrent'] += 1
+            continue
+
+        for k in ['finished', 'progressing']:
+            if fr['torrent_hash'] in remote_hashes[k]: counts[k] += 1
+            L.info(f"{fr['torrent_hash']} for in {k}, skipping!")
+
+        transfer_paths = paths.translate_paths(fr['fastresume']['save_path'], path_specs)
+
+        if not transfer_paths: continue
+
+        renames = itertools.repeat(None)
+
+        if 'mapped_files' in fr['fastresume']:
+            renames = fr['fastresume']['mapped_files']
+
+        for torrent_file, rename in zip(fr['torrent'].files, renames):
+
+            file_path_rel = pathlib.Path(torrent_file.name)
+
+            rename_path_rel = file_path_rel
+            if rename: 
+                rename_path_rel = pathlib.Path(rename)
+
+                L.info(f'Local Rename: {file_path_rel} => {rename_path_rel}')
+
+            file_path_local = transfer_paths['local'] / rename_path_rel
+            file_path_remote = transfer_paths['remote'] / file_path_rel
+
+            L.info(f"Sending: {file_path_local} => {file_path_remote}")
+
+            if not file_path_local.exists():
+                L.warn(f"File: {file_path_local} not found!")
+                continue
+
+            actual_path_size = file_path_local.stat().st_size
+            expected_path_size = torrent_file.length
+
+            if actual_path_size != expected_path_size:
+                L.warn(f"File Size Mismatch: {file_path_local} actual: {actual_path_size} != expected: {expected_path_size}")
+                continue
+            L.info(f"for {file_path_local} sizes seem to match")
+
+            #with progressbar.ProgressBar(widgets = progress_widgets , max_value = torrent_file.length) as progress:
+                # sftp.put(file_path_local, file_path_remote, callback = update_progress, confirm = True)
+
 
 fastresumes = fastresume.parse_all_fastresumes(bt_backup_p)
 
@@ -34,65 +95,8 @@ ssh_client.connect(
 
 sftp = ssh_client.open_sftp()
 
-counts = {
-    'finished': 0,
-    'progressing': 0,
-    'fastresume_problem': 0,
-    'total_tried': 0,
-    'missing_local_torrent': 0
-    }
 
 
-for fr in fastresumes:
-    counts['total_tried'] += 1
-    if not fr:
-        counts['fastresume_problem'] += 1
-        continue
-    if not 'torrent' in fr or not fr['torrent']:
-        counts['missing_local_torrent'] += 1
-        continue
-
-    for k in ['finished', 'progressing']:
-        if fr['torrent_hash'] in remote_hashes[k]: counts[k] += 1
-
-    transfer_paths = paths.translate_paths(fr['fastresume']['save_path'], path_specs)
-
-    if not transfer_paths: continue
-
-    renames = itertools.repeat(None)
-
-    if 'mapped_files' in fr['fastresume']:
-        renames = fr['fastresume']['mapped_files']
-
-    for torrent_file, rename in zip(fr['torrent'].files, renames):
-
-        file_path_rel = pathlib.Path(torrent_file.name)
-
-        rename_path_rel = file_path_rel
-        if rename: 
-            rename_path_rel = pathlib.Path(rename)
-
-            L.info(f'Local Rename: {file_path_rel} => {rename_path_rel}')
-
-        file_path_local = transfer_paths['local'] / rename_path_rel
-        file_path_remote = transfer_paths['remote'] / file_path_rel
-
-        L.info(f"Sending: {file_path_local} => {file_path_remote}")
-
-        if not file_path_local.exists():
-            L.warn(f"File: {file_path_local} not found!")
-            continue
-
-        actual_path_size = file_path_local.stat().st_size
-        expected_path_size = torrent_file.length
-
-        if actual_path_size != expected_path_size:
-            L.warn(f"File Size Mismatch: {file_path_local} actual: {actual_path_size} != expected: {expected_path_size}")
-            continue
-        L.info(f"for {file_path_local} sizes seem to match")
-
-        #with progressbar.ProgressBar(widgets = progress_widgets , max_value = torrent_file.length) as progress:
-            # sftp.put(file_path_local, file_path_remote, callback = update_progress, confirm = True)
 
 
 print(f"{counts}")
