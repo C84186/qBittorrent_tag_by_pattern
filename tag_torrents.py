49b039af9aee675c7af0564c205d8bd007d6c5ef
Smitten <77771103+C84186@users.noreply.github.com>
2021-06-25 19:41:16 +1000

    break tag_torrents into functions slightly

--- tag_torrents.py
+++ tag_torrents.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 tag_hashes = {}
 
-dry_run = False 
+dry_run = True
 
 credentials_path = defs.credentials_path
 tag_for_invalid_overlap = defs.tag_for_invalid_overlap
@@ -28,120 +28,116 @@ if 'category' in tag_map:
         category = tag_map['category']
 
 
+def tag_torrents(torrent_list, qbt_client):
+    # display qBittorrent info
+    print(f'qBittorrent: {qbt_client.app.version}')
+    print(f'qBittorrent Web API: {qbt_client.app.web_api_version}')
+    for k,v in qbt_client.app.build_info.items(): print(f'{k}: {v}')
 
-# display qBittorrent info
-print(f'qBittorrent: {qbt_client.app.version}')
-print(f'qBittorrent Web API: {qbt_client.app.web_api_version}')
-for k,v in qbt_client.app.build_info.items(): print(f'{k}: {v}')
+    untagged_hashes = set()
+    skipped_hashes = set()
 
-untagged_hashes = set()
-skipped_hashes = set()
+    # retrieve and show all torrents
+    for torrent in torrent_list:
 
-all_torrents =  qbt_client.torrents_info()
-# retrieve and show all torrents
-for torrent in all_torrents:
+        print(f"processing: {torrent.name}")
+        current = {}
 
-    print(f"processing: {torrent.name}")
-    current = {}
+        current['torrent'] = torrent#{'name' : torrent.name, 'hash' : torrent.hash}
 
-    current['torrent'] = torrent#{'name' : torrent.name, 'hash' : torrent.hash}
+        current['files'] = list(qbt_client.torrents_files(torrent.hash))
+        # https://qbittorrent-api.readthedocs.io/en/latest/apidoc/torrents.html#qbittorrentapi.torrents.TorrentsAPIMixIn.torrents_create_tags
+        # https://qbittorrent-api.readthedocs.io/en/latest/apidoc/torrents.html#qbittorrentapi.torrents.TorrentDictionary.add_tags
 
-    current['files'] = list(qbt_client.torrents_files(torrent.hash))
-    # https://qbittorrent-api.readthedocs.io/en/latest/apidoc/torrents.html#qbittorrentapi.torrents.TorrentsAPIMixIn.torrents_create_tags
-    # https://qbittorrent-api.readthedocs.io/en/latest/apidoc/torrents.html#qbittorrentapi.torrents.TorrentDictionary.add_tags
+        current_tags = set()
+        
+        print(f" category: {torrent['category']}")
 
-    current_tags = set()
-    
-    print(f" category: {torrent['category']}")
+        process_torrent = False
+        if not category:
+            process_torrent = True
+        else:
+            if category == uncat_kw:
+                if not 'category' in torrent or not torrent['category']: process_torrent = True
+            elif torrent['category'] == category: process_torrent = True
 
-    process_torrent = False
-    if not category:
-        process_torrent = True
-    else:
-        if category == uncat_kw:
-            if not 'category' in torrent or not torrent['category']: process_torrent = True
-        elif torrent['category'] == category: process_torrent = True
+        if not process_torrent:
+            print(f" - skipping as {torrent['category']} != {category}")
+            skipped_hashes.add(torrent['hash'])
+            continue
+        
+        for label in tag_map['labels']:
+            print(f" - processing {label['name']}")
+            if 'extension' in label and label['extension']:
+                print(f"  - processing extensions for {label['name']}")
+                
+                matched_extension = False
+                for ext in label['extension']:
+                    ext = f".{ext}".lower()
 
-    if not process_torrent:
-        print(f" - skipping as {torrent['category']} != {category}")
-        skipped_hashes.add(torrent['hash'])
-        continue
-    
-    for label in tag_map['labels']:
-        print(f" - processing {label['name']}")
-        if 'extension' in label and label['extension']:
-            print(f"  - processing extensions for {label['name']}")
-            
-            matched_extension = False
-            for ext in label['extension']:
-                ext = f".{ext}".lower()
+                    for path in current['files']:
+                        path = path['name']
+                        path = Path(path)
+                        if path.suffix.lower() == ext:
+                            print(f"    - found '{ext}' in {path.name}")
+                            current_tags.add(label['name'])
 
-                for path in current['files']:
-                    path = path['name']
-                    path = Path(path)
-                    if path.suffix.lower() == ext:
-                        print(f"    - found '{ext}' in {path.name}")
+                            matched_extension = True
+
+
+                            if not label['name'] in tag_hashes:
+                                tag_hashes[label['name']] = set()
+
+                            tag_hashes[label['name']].add(torrent['hash'])
+
+            if 'regex' in label and label['regex']:
+                print(f"  - processing regex for {label['name']}")
+
+                for re_pat in label['regex']:
+                    m = re.fullmatch(re_pat, torrent['name'])
+
+                    if m:
+                        print(f"    - found match for regex: '{re_pat}'")
                         current_tags.add(label['name'])
-
-                        matched_extension = True
-
 
                         if not label['name'] in tag_hashes:
                             tag_hashes[label['name']] = set()
 
                         tag_hashes[label['name']].add(torrent['hash'])
 
-        if 'regex' in label and label['regex']:
-            print(f"  - processing regex for {label['name']}")
+        exclusive_overlap = exclusive_labels.intersection(current_tags)
+        print(current_tags)
+        print(exclusive_labels)
+        print(exclusive_overlap)
+        if len(exclusive_overlap) > 1:
+            print(f" - {torrent['name']} matches too many tags: {exclusive_overlap}")
 
-            for re_pat in label['regex']:
-                m = re.fullmatch(re_pat, torrent['name'])
+            current_tags.add(tag_for_invalid_overlap)
 
-                if m:
-                    print(f"    - found match for regex: '{re_pat}'")
-                    current_tags.add(label['name'])
+        current['new_tags'] = list(current_tags)
 
-                    if not label['name'] in tag_hashes:
-                        tag_hashes[label['name']] = set()
+        print(f" - new tags: {current_tags}")
 
-                    tag_hashes[label['name']].add(torrent['hash'])
+        if not dry_run and len(current_tags):
+            existing_tags = set(torrent['tags'])
 
-    exclusive_overlap = exclusive_labels.intersection(current_tags)
-    print(current_tags)
-    print(exclusive_labels)
-    print(exclusive_overlap)
-    if len(exclusive_overlap) > 1:
-        print(f" - {torrent['name']} matches too many tags: {exclusive_overlap}")
+            if current_tags != existing_tags:
+                torrent.add_tags(list(current_tags))
 
-        current_tags.add(tag_for_invalid_overlap)
+        if not len(current_tags):
+            paths = [Path(x['name']).name for x in current['files']]
 
-    current['new_tags'] = list(current_tags)
+            print(f"NO_TAG_NAME: {torrent.name}")
+            print(f"NO_TAG_FILES: files: {paths}")
+            print(f"NO_TAG_HASH: {torrent['hash']}")
 
-    print(f" - new tags: {current_tags}")
+            untagged_hashes.add(torrent['hash'])
 
-    if not dry_run and len(current_tags):
-        existing_tags = set(torrent['tags'])
+    return untagged_hashes, skipped_hashes
 
-        if current_tags != existing_tags:
-            torrent.add_tags(list(current_tags))
-
-    if not len(current_tags):
-        paths = [Path(x['name']).name for x in current['files']]
-
-        print(f"NO_TAG_NAME: {torrent.name}")
-        print(f"NO_TAG_FILES: files: {paths}")
-        print(f"NO_TAG_HASH: {torrent['hash']}")
-
-        untagged_hashes.add(torrent['hash'])
-
-
-print(f"Number of untagged torrents: {len(untagged_hashes)}")
-print(f"Number of skipped torrents: {len(skipped_hashes)}")
-print(f"Number of torrents: {len(all_torrents)}")
-    #  print(json.dumps(current, sort_keys=True, indent=4))
-
-#  for tag in tag_hashes:
-#      print(f"Number of torrents for {tag}: {len(tag_hashes[tag])}")
-#
-#      if not dry_run: qbt_client.torrents_add_tags(tags = tag, torrent_hashes = list(tag_hashes[tag]))
-#
+if __name__ == "__main__":
+    all_torrents =  qbt_client.torrents_info()
+    untagged_hashes, skipped_hashes = tag_torrents(torrent_list, qbt_client) 
+    print(f"Number of untagged torrents: {len(untagged_hashes)}")
+    print(f"Number of skipped torrents: {len(skipped_hashes)}")
+    print(f"Number of torrents: {len(all_torrents)}")
