2663298b5eb7341bbea6c5d6952ffae64495e33c
Smitten <77771103+C84186@users.noreply.github.com>
2021-07-28 22:30:10 +1000

    simplify entrypoint script again

--- synchronize_entrypoint.sh
+++ synchronize_entrypoint.sh
@@ -1,17 +1,8 @@
 #!/bin/sh
 
-echo "Initital synchronization"
-python /app/synchronize_cat_tags.py
-
-echo "Done initial sync"
-
-
-
 while true; do
+  python /app/synchronize_cat_tags.py
   echo "Waiting $1 seconds..."
   sleep "$1"
-  python /app/synchronize_cat_tags.py
-
 done
-
 
